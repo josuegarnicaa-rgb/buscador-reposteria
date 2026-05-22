3895bc9feff5367035b5d3d18c0f60e06d7b732d
epf-21 <efrainperalta.f@gmail.com>
2026-05-22 10:43:51 -0400

    chore: configurar front con react y mover archivos actuales al back

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl border-2 border-amber-200 rounded-2xl p-2 bg-amber-50'>HOLA MUNOD</h1>
+    </>
+  )
+}
+
+export default App
